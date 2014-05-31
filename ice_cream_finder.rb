8e7f145606139a637405360853ee4aedc67ceedb
Esther S Weon <esther.s.weon@gmail.com>
2014-05-31 13:05:27 -0700

    Added search functionality and comments on APIs, cleaned up directions

--- ice_cream_finder.rb
+++ ice_cream_finder.rb
@@ -2,12 +2,21 @@ require 'addressable/uri'
 require 'json'
 require 'launchy'
 require 'rest-client'
+require 'nokogiri'
 
 def ask_current_location
   puts "Please input your current location:"
-  location = gets.chomp
+  address = gets.chomp
 end
 
+def ask_target
+  puts "Please input what you are looking for:"
+  target = gets.chomp
+end
+
+#Google Geocoding API
+#REQUEST: "http://maps.googleapis.com/maps/api/geocode/output?parameters"
+#Given address, returns latitute and longitude
 def current_location(address)
   current_location_url = Addressable::URI.new(
     :scheme => "https",
@@ -23,7 +32,11 @@ def current_location(address)
   results["results"][0]["geometry"]["location"]
 end
 
-def nearby_locations(lat, lng)
+#Google Places API
+#REQUEST: "https://maps.googleapis.com/maps/api/place/nearbysearch/output?parameters"
+#Given latitude and longitude and search string, finds nearby matching locations
+#Returns [name, lat, lng]
+def nearby_locations(lat, lng, target)
   nearby_url = Addressable::URI.new(
     :scheme => "https",
     :host => "maps.googleapis.com",
@@ -33,7 +46,7 @@ def nearby_locations(lat, lng)
       :location => "#{lat}, #{lng}",
       :radius => "5000",
       :sensor => "false",
-      :keyword => "ice cream",
+      :keyword => target,
       :rank_by => "distance"}
   ).to_s
 
@@ -45,14 +58,17 @@ def nearby_locations(lat, lng)
   [name, lat_lng["lat"], lat_lng["lng"]]
 end
 
-def directions(origin, destination)
+#Google Directions API
+#REQUEST: "http://maps.googleapis.com/maps/api/directions/output?parameters"
+#Given two pairs of latitude and longitude, returns directions
+def directions(origin_lat_lng, destination_name_lat_lng)
   directions_url = Addressable::URI.new(
     :scheme => "https",
     :host => "maps.googleapis.com",
     :path => "maps/api/directions/json",
     :query_values => {
-      :origin => "#{origin["lat"]}, #{origin["lng"]}",
-      :destination => "#{destination[1]}, #{destination[2]}",
+      :origin => "#{origin_lat_lng["lat"]}, #{origin_lat_lng["lng"]}",
+      :destination => "#{destination_name_lat_lng[1]}, #{destination_name_lat_lng[2]}",
       :sensor => "false",
       :mode => "walking"}
   ).to_s
@@ -61,22 +77,23 @@ def directions(origin, destination)
   results = JSON.parse(response)
 end
 
-
 def print_directions(results, name)
   puts "Directions to #{name}"
+  puts
   puts "Start Address: #{results["routes"][0]["legs"][0]["start_address"]}"
   puts
   steps = results["routes"][0]["legs"][0]["steps"]
-  steps.each do |k, v|
-    puts k["html_instructions"].gsub(/<.+?>/, " ").gsub("  ", " ")
-    puts "for #{k["distance"]["text"]}"
+  steps.each do |key, value|
+      puts Nokogiri::HTML(key["html_instructions"]).text.gsub("Destination", ". Destination") + "."
+      puts "for #{key["distance"]["text"]}"
   end
   puts
   puts "End Address: #{results["routes"][0]["legs"][0]["end_address"]}"
 end
 
 input_cl = ask_current_location
-cl = current_location(input_cl)
-dst = nearby_locations(cl["lat"], cl["lng"])
-dir = directions(cl, dst)
-print_directions(dir, dst[0])
+input_target = ask_target
+cl_lat_lng = current_location(input_cl)
+dst_name_lat_lng = nearby_locations(cl_lat_lng["lat"], cl_lat_lng["lng"], input_target)
+dir = directions(cl_lat_lng, dst_name_lat_lng)
+print_directions(dir, dst_name_lat_lng[0])
